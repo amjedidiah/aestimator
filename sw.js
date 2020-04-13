f506f681c18e81bc783cfc15da4be57e259e0f72
amjedidiah <imunacode@gmail.com>
2020-04-14 00:51:10 +0100

    Challenge 4 Attempt 30: Hopeful cache && sw fix

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 var CACHE_NAME = "my-site-cache-v1";
 var urlsToCache = [
-  "/aestimator",
-  "/aestimator/js/vendor/jquery-3.4.1.min.js",
-  "/aestimator/js/vendor/modernizr-3.8.0.min.js",
-  "/aestimator/js/main.js",
-  "/aestimator/js/plugins.js",
-  "/aestimator/css/master.css",
-  "/aestimator/css/normalize.min.css",
+  "https://amjedidiah.github.io/aestimator",
+  "https://amjedidiah.github.io/aestimator/js/vendor/jquery-3.4.1.min.js",
+  "https://amjedidiah.github.io/aestimator/js/vendor/modernizr-3.8.0.min.js",
+  "https://amjedidiah.github.io/aestimator/js/main.js",
+  "https://amjedidiah.github.io/aestimator/js/plugins.js",
+  "https://amjedidiah.github.io/aestimator/css/master.css",
+  "https://amjedidiah.github.io/aestimator/css/normalize.min.css",
 ];
 
 self.addEventListener("install", function (event) {
