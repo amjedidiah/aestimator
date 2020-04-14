0a3ef4af88d71d3481e69a5c5967571cef95b21e
amjedidiah <imunacode@gmail.com>
2020-04-14 01:41:11 +0100

    Challenge 4 Attempt 37: Cache && sw fix

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 const filesToCache = [
-  "https://amjedidiah.github.io/aestimator",
-  "https://amjedidiah.github.io/aestimator/js/vendor/jquery-3.4.1.min.js",
-  "https://amjedidiah.github.io/aestimator/js/vendor/modernizr-3.8.0.min.js",
-  "https://amjedidiah.github.io/aestimator/js/main.js",
-  "https://amjedidiah.github.io/aestimator/js/plugins.js",
-  "https://amjedidiah.github.io/aestimator/css/master.css",
-  "https://amjedidiah.github.io/aestimator/css/normalize.min.css",
+  "aestimator",
+  "aestimator/js/vendor/jquery-3.4.1.min.js",
+  "aestimator/js/vendor/modernizr-3.8.0.min.js",
+  "aestimator/js/main.js",
+  "aestimator/js/plugins.js",
+  "aestimator/css/master.css",
+  "aestimator/css/normalize.min.css",
 ];
 
 const staticCacheName = "pages-cache-v1";
